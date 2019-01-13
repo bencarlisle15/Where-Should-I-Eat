5e274ba5b1ecb253285601c5a9fd18aa004c46ca
Ben Carlisle <bencarlisle15@gmail.com>
2019-01-13 14:18:22 -0500

    Updated to 28

--- app/src/main/java/com/carlisle/ben/whereshouldieat/MainActivity.java
+++ app/src/main/java/com/carlisle/ben/whereshouldieat/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -38,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		System.out.print(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION));
+		ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION);
 		setContentView(R.layout.main_layout);
 		AdView mAdView = findViewById(R.id.adView);
 		AdRequest adRequest = new AdRequest.Builder().build();
@@ -99,8 +98,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 
 	@Nullable
 	private Location getLastBestLocation() {
-		if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-				ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 			return null;
 		}
 		Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -153,11 +151,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 						choice[0] = true;
 					}
 				})
-				.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						choice[0] = false;
-					}
-				})
+				.setNegativeButton(android.R.string.no, (dialog, which) -> choice[0] = false)
 				.setIcon(android.R.drawable.ic_dialog_alert)
 				.show();
 		return choice[0];
@@ -173,11 +167,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 	private void ads(final MenuItem i) {
 		if (adsOn) {
 			AlertDialog.Builder builder;
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-				builder = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert);
-			} else {
-				builder = new AlertDialog.Builder(this);
-			}
+			builder = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert);
 			builder.setTitle("Do you need to remove ads?")
 					.setMessage("I'm trying to pay for college so please keep ads on if possible thanks.")
 					.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
@@ -185,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 							adsOn = false;
 							i.setTitle(R.string.on);
 							findViewById(R.id.adView).setVisibility(View.INVISIBLE);
-							RelativeLayout layouts = (RelativeLayout) findViewById(R.id.layouts);
+							RelativeLayout layouts = findViewById(R.id.layouts);
 							RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) layouts.getLayoutParams();
 							params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 							layouts.setLayoutParams(params);
@@ -201,7 +191,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
 			adsOn = true;
 			findViewById(R.id.adView).setVisibility(View.VISIBLE);
 			i.setTitle(R.string.off);
-			RelativeLayout l = (RelativeLayout) findViewById(R.id.layouts);
+			RelativeLayout l = findViewById(R.id.layouts);
 			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) l.getLayoutParams();
 			params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
 			l.setLayoutParams(params);
